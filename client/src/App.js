086673a9a0f2c392a157005d988be72f6da3a826
Bibek Gaire <gairebibek8@gmail.com>
2024-09-23 14:43:33 -0500

    Created navbar and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import { ThemeProvider, styled } from "styled-components";
 import { lightTheme } from "./utils/Themes";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Authentication from "./pages/Authentication";
+import { useState } from "react";
+// import { useSelector } from "react-redux";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import Workouts from "./pages/Workouts";
+// import Dashboard from "./pages/Dashboard";
+// import Workouts from "./pages/Workouts";
 
 const Container = styled.div`
   width: 100%;
@@ -16,12 +23,23 @@ const Container = styled.div`
 `;
 
 function App() {
+  const [user, setUser] = useState(true);
   return (
     <ThemeProvider theme={lightTheme}>
       <BrowserRouter>
+        {user ? (
+          <Container>
+            <Navbar/>
+            <Routes>
+              <Route path="/" exact element={<Dashboard />}/>
+              <Route path="/workouts" exact element={<Workouts />}/>
+            </Routes>
+          </Container>
+        ) : (
           <Container>
             <Authentication />
           </Container>
+        )}
       </BrowserRouter>
     </ThemeProvider>
   );
---
+++ client/src/pages/Workouts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Workouts = () => {
+  return (
+    <div>
+      Workout
+    </div>
+  )
+}
+
+export default Workouts
